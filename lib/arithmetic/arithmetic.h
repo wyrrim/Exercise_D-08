77fab1791db391010242545eb46be05c9e0dfc43
Sergey Kalinichenko <sergey.d.kalinichenko@gmail.com>
2021-04-20 13:40:15 +0200

    add_double

--- lib/arithmetic/arithmetic.h
+++ lib/arithmetic/arithmetic.h
@@ -1,6 +1,8 @@
 #ifndef ARITHMETIC_H
 #define ARITHMETIC_H
 
-int arithmetic_add(const int x, const int y);
+int arithmetic_add_int(const int x, const int y);
+
+int arithmetic_add_double(const double x, const double y);
 
 #endif /*ARITHMETIC_H*/
